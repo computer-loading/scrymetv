b18caee9f39825157211c12b0aeaeb59fd18e58f
IzzyJamess / Guillaume.G <guillaume.gouhier1999@gmail.com>
2018-05-15 18:54:45 +0200

    update specs

--- background.js
+++ background.js
@@ -2,6 +2,7 @@ var tickRate = 60000 // On vérifiera l'api toutes les minutes
 
 function checkStream() {
   var xhr = new XMLHttpRequest()
+  var OnStream = 
   xhr.open("GET", "https://api.twitch.tv/kraken/streams/frenchycommunity?client_id=wjhv3b2zg2sc51s1qj9a6emuy8le7s", true)
   xhr.onreadystatechange = function () {
     if(xhr.readyState == 4) {
